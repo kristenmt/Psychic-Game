92194cd4539ec39720ccd136dddbaf16cd36fefb
Kristen Truyol <kristentruyol@Kristens-Air.centurylink.net>
2018-07-17 22:56:37 -0400

    linked css and js files to index, pseudo coded game in js file

---
+++ Assets/Javascript/game.js
@@ -0,0 +1,25 @@
+//display wins
+
+//display losses
+
+//display # of guesses left
+
+//the letters user has guessed so far
+
+//when user wins, game will start over again
+
+//when user loses, game will start over again
+
+//need to define options computer will guess from
+
+//need to let computer know to measure its guess against users guess when user touch/release key
+
+//need to define what gets a point and what loses a point
+
+//need to define how many guesses user will be allowed (loop)
+
+//need to show the user the guesses already made on the html
+
+//determine win value and loss value
+
+//reset game when the game is over
